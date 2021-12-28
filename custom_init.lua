787f079a140f8bc8e6073f494d902c6fc8368af4
System Administrator <root@lizhonghandeMacBook-Pro.local>
2021-12-28 11:30:17 +0000

    first commit

---
+++ custom_init.lua
@@ -0,0 +1,14 @@
+-- 设置屏幕中间竖条
+-- vim.o.colorcolumn = "130"
+
+-- 禁用多光标警告
+vim.g.VM_show_warnings = 0
+
+-- 设置asynctasks窗口排版
+vim.g.asynctasks_term_pos = 'bottom'
+vim.g.asynctasks_term_rows = 15
+
+-- vim-table-mode更改为markdown语法
+vim.g.table_mode_corner='|'
+-- 设置初始为透明背景
+vim.g.transparent_enabled = true

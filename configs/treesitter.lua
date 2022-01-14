76c095c5493067aca5b830dd57f0b834138b62cd
李中瀚 <lizhonghan@lizhonghandeMacBook-Pro.local>
2022-01-14 00:47:18 +0000

    key mappings for workman layout

---
+++ configs/treesitter.lua
@@ -0,0 +1,20 @@
+local present, ts_config = pcall(require, "nvim-treesitter.configs")
+
+if not present then
+   return
+end
+
+ts_config.setup {
+   ensure_installed = {
+      "lua",
+      "vim",
+      "go",
+      "c",
+      "cpp",
+      "html",
+   },
+   highlight = {
+      enable = true,
+      use_languagetree = true,
+   },
+}
